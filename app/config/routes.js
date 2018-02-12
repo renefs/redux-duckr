245e9a3b8ebe8a6e481f3d7d11a1841f96c19105
Rene Fernandez Sanchez <rene.fernandez@cern.ch>
2018-02-12 14:27:00 +0100

    Add home route and Navigation

--- app/config/routes.js
+++ app/config/routes.js
@@ -1,10 +1,12 @@
 import React from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
-import { MainContainer } from 'containers'
+import { Router, Route, hashHistory, IndexRoute } from 'react-router'
+import { MainContainer, HomeContainer } from 'containers'
 
 const routes = (
-  <Router>
-    <Route path='/' component={MainContainer} />
+  <Router history={hashHistory}>
+    <Route path='/' component={MainContainer}>
+      <IndexRoute component={HomeContainer} />
+    </Route>
   </Router>
 )
 
--- app/index.js
+++ app/index.js
@@ -1,4 +1,5 @@
 import ReactDOM from 'react-dom'
-import routes from './config/routes'
+import routes from 'config/routes'
 
 ReactDOM.render(routes, document.getElementById('app'))
+
